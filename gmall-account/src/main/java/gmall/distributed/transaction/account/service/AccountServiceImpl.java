e5ed975341abcf258fb746a7bbaac586e1966ad7
haibozhang <haibozhang2019@163.com>
2022-10-08 13:09:51 +0800

    代码修改

--- gmall-account/src/main/java/gmall/distributed/transaction/account/service/AccountServiceImpl.java
+++ gmall-account/src/main/java/gmall/distributed/transaction/account/service/AccountServiceImpl.java
@@ -9,6 +9,7 @@ import gmall.distributed.transaction.common.response.ObjectResponse;
 import io.seata.spring.annotation.GlobalLock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
